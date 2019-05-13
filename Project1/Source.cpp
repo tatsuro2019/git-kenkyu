60a6559d6c2d09dc518932efc59a0d29725fb58a
達郎 宮崎 <6BDI2225@cc.u-tokai.ac.jp>
2019-05-13 19:38:18 +0900

    プロジェクト ファイルを追加します。

---
+++ Project1/Source.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(void)
+{
+	// 画像を格納するオブジェクトを宣言する
+	cv::Mat	image;
+
+	// 画像ファイルから画像データを読み込む
+	image = cv::imread("image/lenna.bmp");
+
+	if (image.empty() == true) {
+		// 画像データが読み込めなかったときは終了する
+		return 0;
+	}
+
+	// ウィンドウに画像を表示する
+	// # ここではウィンドウに「画像」という識別名を付けている
+	cv::imshow("画像", image);
+
+	// 何かキーが押されるまで待つ
+	cv::waitKey();
+
+	return 0;
+}
