9eaa2916ca1b9c84691b62a30d2cfb0895f3257f
達郎 宮崎 <6BDI2225@cc.u-tokai.ac.jp>
2019-05-14 14:57:17 +0900

    5/14

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,26 +1,49 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#include <iostream>
+#include <opencv2/flann/flann.hpp>
+//cvTermCriteriaの定義
+#include <opencv2/core/types_c.h>
+
+#define OPENCV_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
+#define OPENCV_VERSION_CODE OPENCV_VERSION(CV_MAJOR_VERSION, CV_MINOR_VERSION, CV_SUBMINOR_VERSION)
+
 
 using namespace cv;
 
-int main(void)
+int main(int argc, char* argv[])
 {
-	// 画像を格納するオブジェクトを宣言する
-	cv::Mat	image;
+	// 入力画像の取得
+	Mat im = imread("image/lenna.png");
+	Mat im2(im.size(), im.type());
+	if (im.empty()) return -1;
 
-	// 画像ファイルから画像データを読み込む
-	image = cv::imread("image/lenna.bmp");
+	// 画像配列を1次元に変換
+	Mat points;
+	im.convertTo(points, CV_32FC3);
+	points = points.reshape(3, im.rows * im.cols);
 
-	if (image.empty() == true) {
-		// 画像データが読み込めなかったときは終了する
-		return 0;
+	// RGB空間でk-meansクラスタリングを実行
+	Mat_<int> clusters(points.size(), CV_32SC1);
+	Mat centers;
+
+	// クラスタ数
+	const int cluster = 5;
+	// k-meansnクラスタリングの実行
+	kmeans(points, cluster, clusters, cvTermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 10, 1.0), 1, KMEANS_PP_CENTERS, centers);
+
+	// 画素値をクラスタ中心値に置換
+	MatIterator_<Vec3b> itd = im2.begin<Vec3b>(), itd_end = im2.end<Vec3b>();
+	for (int i = 0; itd != itd_end; ++itd, ++i) {
+		Vec3f& color = centers.at<Vec3f>(clusters(i), 0);
+		(*itd)[0] = saturate_cast<uchar>(color[0]);
+		(*itd)[1] = saturate_cast<uchar>(color[1]);
+		(*itd)[2] = saturate_cast<uchar>(color[2]);
 	}
-
-	// ウィンドウに画像を表示する
-	// # ここではウィンドウに「画像」という識別名を付けている
-	cv::imshow("画像", image);
-
-	// 何かキーが押されるまで待つ
-	cv::waitKey();
-
-	return 0;
+	// 結果表示
+	imshow("Input", im);
+	imshow("Output", im2);
+	waitKey(0);
 }
